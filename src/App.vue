251030985dd69afe20f8f73b4819da7b5553d190
Kelly C Santos <kellycristinasntos@gmail.com>
2022-02-09 17:59:25 -0300

    primeiro commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Bem vindo(a) a minha segunda aplicação Vue.js"/>
+ 
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+body{
+  background: #abdac5;
+  }
+</style>
